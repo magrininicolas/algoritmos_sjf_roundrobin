09ad0c8d473bafd829cf06f0059b225eed91fe5d
Nicolas <magrininicolas@outlook.com>
2024-04-12 12:42:12 -0300

    Add algoritmo que simula um escalonamento SJF

---
+++ sjf/algoritmo.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+)
+
+const TAM = 10
+
+type Processo struct {
+	Nome          string
+	Tempo, Te, Tr int
+}
+
+func Inserir(prontos []Processo) []Processo {
+	if len(prontos) == TAM {
+		fmt.Println("Lista de processos cheia")
+		return prontos
+	}
+	var novoProcesso Processo
+	fmt.Print("Entre com o nome do processo: ")
+	fmt.Scanf("%s", &novoProcesso.Nome)
+	fmt.Print("Entre com o tempo de processamento: ")
+	fmt.Scanf("%d", &novoProcesso.Tempo)
+	prontos = append(prontos, novoProcesso)
+	return prontos
+}
+
+func Show(prontos []Processo) {
+	if len(prontos) == 0 {
+		fmt.Println("Lista de processos vazia")
+		return
+	}
+	for _, p := range prontos {
+		fmt.Println("--------------------------------------------")
+		fmt.Println("Nome do processo:", p.Nome)
+		fmt.Println("Tempo de processamento:", p.Tempo)
+		fmt.Println("--------------------------------------------")
+	}
+}
+
+func Menu() int {
+	op := 0
+	fmt.Println("Selecione uma opção do menu")
+	fmt.Println("1 - Inserir processo")
+	fmt.Println("2 - Mostrar lista de processos")
+	fmt.Println("3 - SJF")
+	fmt.Println("4 - Sair")
+	fmt.Scan(&op)
+	return op
+}
+
+func Sjf(prontos []Processo) {
+	sortProcessos(prontos)
+	var espera int
+	var retorno int
+	for i := 0; i < len(prontos); i++ {
+		if i == 0 {
+			espera = 0
+		} else {
+			espera = prontos[i-1].Te + prontos[i-1].Tempo
+		}
+		retorno = espera + prontos[i].Tempo
+		prontos[i].Te = espera
+		prontos[i].Tr = retorno
+	}
+	showProntos(prontos)
+	showMedia(prontos)
+}
+
+func calculaTotal(prontos []Processo) (int, int) {
+	var totEsp int
+	var totRet int
+	for _, v := range prontos {
+		totEsp += v.Te
+		totRet += v.Tr
+	}
+	return totEsp, totRet
+}
+
+func showMedia(prontos []Processo) {
+	totEsp, totRet := calculaTotal(prontos)
+	tme := float64(totEsp) / float64(len(prontos))
+	tmr := float64(totRet) / float64(len(prontos))
+	fmt.Printf("Tempo médio de espera: %d/%d = %.4f\n", totEsp, len(prontos), tme)
+	fmt.Printf("Tempo médio de retorno: %d/%d = %.4f\n", totRet, len(prontos), tmr)
+}
+
+func sortProcessos(prontos []Processo) {
+	menor := func(i, j int) bool {
+		return prontos[i].Tempo < prontos[j].Tempo
+	}
+
+	sort.Slice(prontos, menor)
+}
+
+func showProntos(prontos []Processo) {
+	if len(prontos) == 0 {
+		fmt.Println("Não existem processos prontos para execução")
+	}
+	for _, p := range prontos {
+		fmt.Println("--------------------------------------------")
+		fmt.Println("Processo:", p.Nome)
+		fmt.Println("Tempo de execução", p.Tempo)
+		fmt.Println("Tempo de espera:", p.Te)
+		fmt.Println("Tempo de turnaround: ", p.Tr)
+		fmt.Println("--------------------------------------------")
+	}
+}
+
+func main() {
+	var op int
+	var prontos []Processo
+	for ok := true; ok; ok = (op != 4) {
+		op = Menu()
+		switch op {
+		case 1:
+			prontos = Inserir(prontos)
+		case 2:
+			Show(prontos)
+		case 3:
+			Sjf(prontos)
+		case 4:
+			os.Exit(0)
+		default:
+			fmt.Println("Digite uma opção válida")
+		}
+	}
+}
---
+++ sjf/go.mod
@@ -0,0 +1,3 @@
+module sjf
+
+go 1.22.1
